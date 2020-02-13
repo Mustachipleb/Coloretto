8f9b79ed44c8c9abd04e9b833a2563776129c358
Nicolas Van Damme <nicolas.van.damme2@hotmail.com>
2020-02-13 16:46:27 +0100

    Updated diagrams
    domeinmodel aangepast gebaseerd op Dev branch.
    Use Case Diagram opgesteld.

--- src/domein/Spel.java
+++ src/domein/Spel.java
@@ -4,10 +4,21 @@ import java.util.*;
 
 public class Spel {
 
-	private Collection<Speler> spelers;
-	private Collection<Kaart> kaarten;
-
-		private Collection<Deck> spelerdecks;
-
+	private List<Speler> spelers;
+	private Deck startDeck;
 	private Ronde huidigeRonde;
+
+	/**
+	 * Start een nieuwe Ronde: Bepaal spelers en voer de initilisatie van een Ronde uit (Lijst van spelregels om een ronde te spelen) en andere operaties.
+	 */
+	public Ronde StartNieuweRonde(SpelRegel[] spelRegels) {
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * Constructor
+	 */
+	public Spel Spel() {
+		throw new UnsupportedOperationException();
+	}
 }
