785778fc8380e35b9328d898c3b839c3ebfc5c2d
Nicolas Van Damme <37940247+Mustachipleb@users.noreply.github.com>
2020-05-03 15:22:46 +0200

    added connection sample

--- src/main/StartUp.java
+++ src/main/StartUp.java
@@ -6,6 +6,9 @@ import javafx.event.EventHandler;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 
 import gui.SpelScherm;
@@ -15,6 +18,8 @@ import gui.WelkomScherm;
 public class StartUp extends Application
 {
 	Scene welkomScherm, spelScherm;
+	
+	private static final String JDBC_URL = "jdbc:mysql://127.0.0.1:3306/coloretto?user=user&password=NFohFZx3Y3L7AH/&serverTimezone=UTC&useSSL=false";
 	
 	@Override
 	public void start(Stage primaryStage) {
@@ -31,6 +36,16 @@ public class StartUp extends Application
 				}
 			}
 		});
+		
+		try (Connection conn = DriverManager.getConnection(JDBC_URL))
+		{
+			System.out.println("U good");
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+		
 		
 		Scene welkomScherm = new Scene(root, 500, 250);
 		welkomScherm.getStylesheets().add(getClass().getResource("/css/main.css").toExternalForm());
